dfa65a7479eed877d81fc529a77cac4a6e0d5efa
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-10-19 11:27:19 +0200

    Add Base Layout

---
+++ src/layouts/baseLayout.tsx
@@ -0,0 +1,15 @@
+﻿import Header from '../components/header/header'
+import './baseLayout.scss'
+
+type Props = {
+  children: JSX.Element
+}
+
+const BaseLayout = ({ children }: Props) => (
+  <div className="BodyContainer">
+    <Header />
+    {children}
+  </div>
+)
+
+export default BaseLayout
---
+++ src/layouts/index.ts
@@ -0,0 +1,1 @@
+﻿export { default as BaseLayout } from './baseLayout'
