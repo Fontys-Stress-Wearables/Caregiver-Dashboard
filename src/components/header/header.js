f5de6da4312e887a1d9e84feaed271e40511dcb9
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-10-19 11:24:44 +0200

    Style Header and Welcome

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import './header.css'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button'
 import LogoutIcon from '@mui/icons-material/Logout'
 import SWSPLogo from '../../assets/swsp-logo.svg'
 
 function Header() {
-  const navigate = useNavigate()
   return (
-    <div className="Header">
-      <div className="Logo" role="presentation" onClick={() => navigate('/')}>
-        <img src={SWSPLogo} alt="SWSP Logo" />
+    <div className="Header-Container">
+      <div className="Header">
+        <Link to="/" className="Logo">
+          <img src={SWSPLogo} alt="SWSP Logo" className="Logo-Image" />
+        </Link>
+        <div className="Logout">
+          <Button
+            size="large"
+            color="error"
+            variant="contained"
+            startIcon={<LogoutIcon />}
+            onClick={() => {
+              alert('clicked on LogOut')
+            }}
+          >
+            Logout
+          </Button>
+        </div>
       </div>
-      <div className="Logout">
-        <Button
-          color="error"
-          variant="contained"
-          startIcon={<LogoutIcon />}
-          onClick={() => {
-            alert('clicked on LogOut')
-          }}
-        >
-          Logout
-        </Button>
-      </div>
+      <hr className="Header-Separator" />
     </div>
   )
 }
