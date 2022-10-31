f723b3b736d4ed5d884e38cff7ff06773bf1c6ff
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-10-31 12:55:43 +0100

    Set patient name

--- src/components/title/titleWelcome.js
+++ src/components/title/titleWelcome.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './titleWelcome.css'
+import { useMsal } from '@azure/msal-react'
 
 function TitleWelcome() {
+  const { instance } = useMsal()
+
   return (
     <div className="WelcomeTitle">
       <h1>Caregiver Dashboard</h1>
-      <h4>Welcome Caregiver</h4>
+      <h4>Welcome {instance.getActiveAccount().name}</h4>
     </div>
   )
 }
