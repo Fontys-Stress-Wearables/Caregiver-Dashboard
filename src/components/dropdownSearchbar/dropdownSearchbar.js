1432ce211a671395683f5eef6a108fea4ab1afe5
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-10-31 12:57:04 +0100

    Add logged API call

--- src/components/dropdownSearchbar/dropdownSearchbar.js
+++ src/components/dropdownSearchbar/dropdownSearchbar.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
 import './dropdownSearchbar.css'
+import { useMsal } from '@azure/msal-react'
+import { AUTH_REQUEST_SCOPE_URL } from '../../utils/environment'
+import { getPatientGroupsForCaregiver } from '../../utils/api/calls'
 
 function DropdownSearchbar() {
   const [selectedOption, setSelectedOption] = useState('Select Patient Group')
+  const { instance, accounts } = useMsal()
 
   const aquaticCreatures = [
     { label: 'Shark', value: 'Shark' },
@@ -14,12 +18,47 @@ function DropdownSearchbar() {
     { label: 'Lobster', value: 'Lobster' },
   ]
 
+  const request = {
+    scopes: [AUTH_REQUEST_SCOPE_URL, 'User.Read'],
+    account: accounts[0],
+  }
+
+  useEffect(() => {
+    fetchPatientGroups()
+  })
+
+  const fetchPatientGroups = () => {
+    instance
+      .acquireTokenSilent(request)
+      .then((res) => {
+        console.log(res)
+        getPatientGroupsForCaregiver(res.accessToken, res.uniqueId)
+          .then((response) => {
+            if (response.error) {
+              console.log(response)
+            } else {
+              const foundPatientGroups = response.response
+              console.log(foundPatientGroups)
+              // setError(false)
+              // setOrganizations([...foundOrganizations])
+            }
+          })
+          .catch((err) => {
+            console.error('Error occurred while fetching organizations', err)
+          })
+      })
+      .catch((e) => {
+        console.error('Error occurred while fetching patients', e)
+      })
+  }
+
   const handleTypeSelect = (e) => {
     setSelectedOption(e.value)
   }
 
   return (
     <div className="SearchBar">
+      {console.log(instance.getActiveAccount())}
       <div
         style={{
           boxSizing: 'border-box',
