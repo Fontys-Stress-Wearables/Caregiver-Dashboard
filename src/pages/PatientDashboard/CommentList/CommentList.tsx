b38a909b52b91a0802ad6be38d97d9a52d8ecca7
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-12-12 12:32:30 +0100

    Convert PatientDashboard.js to PatientDashboard.tsx and change MockComments.js to MockComments.json

--- src/pages/PatientDashboard/CommentList/CommentList.tsx
+++ src/pages/PatientDashboard/CommentList/CommentList.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { useState } from 'react'
 import { FeedbackProps } from '../../../utils/api/calls'
-import { MockComments } from './MockComments'
+import MockComments from './MockComments.json'
 import Comment from './Comment/Comment'
 import CommentModal from '../../../components/Modals/CommentModal/CommentModal'
 import List from '@mui/material/List'
--- src/pages/PatientDashboard/PatientDashboard.tsx
+++ src/pages/PatientDashboard/PatientDashboard.tsx
@@ -1,7 +1,7 @@
-import Title from './Title/Title.tsx'
-import CommentList from './CommentList/CommentList.tsx'
 import Graph from '../../components/Graph/graph'
 import BaseLayout from '../../layouts/BaseLayout'
+import Title from './Title/Title'
+import CommentList from './CommentList/CommentList'
 
 const PatientDashboard = () => {
   return (
